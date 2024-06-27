c274f9d70b0dc5113471168254de1246dac5f91b
vanhoa690 <vanhoa690@gmail.com>
2024-06-27 15:12:08 +0700

    add form login

--- client/src/pages/Login.tsx
+++ client/src/pages/Login.tsx
@@ -2,10 +2,11 @@ import { useEffect, useState } from "react";
 import Header from "../components/Header";
 import axios from "axios";
 import ProductCard from "../components/ProductCard";
-import { Container, Stack, TextField } from "@mui/material";
+import { Button, Container, Stack, TextField, Typography } from "@mui/material";
 import Loading from "../components/Loading";
 import { Product } from "../types/Product";
 import Flash from "../components/Flash";
+import { Form, Field } from "react-final-form";
 
 function Login() {
   const [products, setProducts] = useState<Product[]>([]);
@@ -29,11 +30,57 @@ function Login() {
     getAllProduct();
   }, []);
 
+  const handleSubmit = async (values: any) => {
+    console.log(values);
+    await axios.post("http://localhost:3000/auth/login", values);
+  };
+  const validate = (values: any) => {
+    const errors: any = {};
+    const { username } = values;
+    if (!username) errors.username = "Vui lòng nhập username";
+    return errors;
+  };
   return (
     <>
       <Container>
-        <TextField id="standard-basic" label="Email" variant="standard" />
-        <TextField id="standard-basic" label="Password" variant="standard" />
+        <Typography variant="h2" mb={3}>
+          Login
+        </Typography>
+        <Form
+          onSubmit={handleSubmit}
+          validate={validate}
+          render={({ values }) => (
+            <Stack maxWidth={"400px"} gap={3}>
+              <Field<string> name="username">
+                {({ input, meta }) => (
+                  <TextField
+                    autoComplete="off"
+                    error={meta.touched && meta.error}
+                    helperText={meta.touched && meta.error}
+                    label="Username"
+                    {...input}
+                  />
+                )}
+              </Field>
+              <Field<string> name="email">
+                {({ input }) => <TextField label="Email" {...input} />}
+              </Field>
+              <Field<string> name="password">
+                {({ input }) => (
+                  <TextField label="Password" {...input} type="password" />
+                )}
+              </Field>
+
+              <Button
+                type="submit"
+                variant="contained"
+                onClick={() => handleSubmit(values)}
+              >
+                Submit
+              </Button>
+            </Stack>
+          )}
+        />
       </Container>
     </>
   );
--- client/src/pages/ProductDetail.tsx
+++ client/src/pages/ProductDetail.tsx
@@ -1,16 +1,14 @@
 import { useEffect, useState } from "react";
-import Header from "../components/Header";
 import axios from "axios";
-import ProductCard from "../components/ProductCard";
-import { Container, Stack } from "@mui/material";
-import Loading from "../components/Loading";
-import { Product } from "../types/Product";
-import Flash from "../components/Flash";
+import { Button, Container, Stack, Typography } from "@mui/material";
+import Loading from "src/components/Loading";
+import { Product } from "src/types/Product";
+import Flash from "src/components/Flash";
 import { useParams } from "react-router-dom";
 
 function ProductDetail() {
   const { id } = useParams();
-  const [product, setProduct] = useState();
+  const [product, setProduct] = useState<Product>();
   const [loading, setLoading] = useState<Boolean>(false);
   const [flash, setFlash] = useState<Boolean>(false);
 
@@ -34,21 +32,37 @@ function ProductDetail() {
     getProductDetail(id);
   }, [id]);
 
+  if (!product) return <Typography>Not Found Product</Typography>;
   return (
     <>
       {loading ? (
         <Loading />
       ) : (
         <>
-\          {flash && <Flash type="error" message="Something errors ..." />}
+          {flash && <Flash type="error" message="Something errors ..." />}
           <Container>
-            <Stack
-              direction={"row"}
-              flexWrap={"wrap"}
-              gap={3}
-              alignContent={"center"}
-            >
-              {product && <ProductCard product={product} />}
+            <Stack direction={"row"} gap={3} alignContent={"center"}>
+              <img src={product.image} width={"450px"} />
+              <Stack gap={2}>
+                <Typography variant="h4">{product.title}</Typography>
+                <Typography variant="body1">{product.description}</Typography>
+                <Typography variant="h5" color={"red"}>
+                  Price: {product.price} USD
+                </Typography>
+                <Typography variant="h5">
+                  Category: {product.category}
+                </Typography>
+                <Typography variant="h5">Votes:</Typography>
+                <Stack>
+                  <Typography variant="caption">
+                    Count: {product.rating.count} rates
+                  </Typography>
+                  <Typography variant="caption">
+                    Rate: {product.rating.rate}
+                  </Typography>
+                </Stack>
+                <Button variant="outlined">Add to Cart</Button>
+              </Stack>
             </Stack>
           </Container>
         </>
