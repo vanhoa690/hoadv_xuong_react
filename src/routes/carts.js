3fb5dfcb3b95214aae3c3e7a80ea872c8cda6cf8
vanhoa690 <vanhoa690@gmail.com>
2024-07-21 17:26:01 +0700

    refactor

--- src/routes/carts.js
+++ src/routes/carts.js
@@ -10,8 +10,10 @@ cartsRouter.get("/:id", cartsController.getCartDetail);
 cartsRouter.get("/user/:id", cartsController.getCartUser);
 cartsRouter.post("/", cartsController.createCart);
 cartsRouter.put("/:id", cartsController.updateCart);
-cartsRouter.put("/product/:id", cartsController.updateProductCart);
 cartsRouter.delete("/:id", cartsController.deleteCart);
-cartsRouter.delete("/product/:id", cartsController.deleteProductCart);
+cartsRouter.delete(
+  "/user/:userId/product/:id",
+  cartsController.deleteProductCart
+);
 
 export default cartsRouter;
