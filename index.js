90336c97d8510027e7f8419fd5481fc008dae4b1
vanhoa690 <vanhoa690@gmail.com>
2024-07-25 14:43:48 +0700

    fix cors

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 import express from "express";
+import cors from "cors";
 import dotenv from "dotenv";
 import router from "./src/routes/index.js";
 import connectMongoDB from "./src/config/dbconfig.js";
 dotenv.config();
 const app = express();
+app.use(cors());
 app.use(
   express.urlencoded({
     extended: true,
